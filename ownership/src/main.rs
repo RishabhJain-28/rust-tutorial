e6ea0baa567cb27974c999297862ee3de60d0b3f
Rishabh Jain <rishabhnjain@gmail.com>
2022-11-13 16:04:31 +0530

    structs

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // let mut s = String::from("hello");
+    // s.push_str(" wordl!");
+    // println!("{s}");
+    // let mut s1 = String::from("sad");
+    // // let r1 = &s1;
+    // let r2 = &mut s1;
+    // // let r3 = &s1;
+    // let r4 = &mut s1;
+
+    let mut s = String::from("hello");
+
+    let r2 = &s;
+    println!("{}", r2);
+    let r1 = &mut s;
+    println!("{}", r1);
+
+    // take_and_return_ownership(&mut s1);
+    // println!("{s1}")
 }
+
+// fn take_and_return_ownership(s1: &mut String) {
+//     s1.push_str("fd");
+// }
--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,8 +1,15 @@
+#[derive(Debug)]
 struct User {
     email: String,
     pass: String,
 }
-struct Color(i32, i32, i32);
+impl User {
+    fn change_email(&mut self, email: &str) {
+        self.email = String::from(email);
+    }
+}
+
+// struct Color(i32, i32, i32);
 
 fn create_user(email: String, pass: String) -> User {
     let u = User { email, pass };
@@ -12,7 +19,13 @@ fn main() {
     let s1 = String::from("email1");
     let s2 = s1.clone();
     let u1 = create_user(s1, String::from("pass1"));
-    let u2 = User { email: s2, ..u1 };
+    let mut u2 = User { email: s2, ..u1 };
     println!("{}", u2.pass);
-    let c = Color(0, 0, 0);
+    println!("{}", u2.email);
+    // let c = Color(0, 0, 0);
+    let z = String::from("emqaill");
+    u2.change_email(&z);
+
+    println!("{:?}", u2);
+    dbg!(&u2);
 }
