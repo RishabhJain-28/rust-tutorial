70d03d5c442cd44c43f20e3421245ffdac03ea5f
Rishabh Jain <rishabhnjain@gmail.com>
2022-11-26 06:27:11 +0530

    crates

---
+++ crates/src/main.rs
@@ -0,0 +1,10 @@
+// --packages
+//   --crates -> lib or binary
+//      --modules
+
+use crate::garden::vegetables::Asparagus;
+pub mod garden;
+
+fn main() {
+    println!("Hello, world!");
+}
--- enums/src/main.rs
+++ enums/src/main.rs
@@ -8,4 +8,7 @@ fn main() {
     let home = IpAddr::V4(String::from("127.0.0.1"));
 
     let loopback = IpAddr::V6(String::from("::1"));
+
+    let some_number = Some(5);
+    let no_number: Option<i32> = None;
 }
